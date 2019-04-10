ba61b353e85545890b6fb0bdef8557f7c0d633ac
GabrielHorbach <gabrielhorbachmachado@hotmail.com>
2019-04-09 23:03:31 -0300

    Bug correction on server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const cosr = require('cors');
+const cors = require('cors');
 
 const app = express();
 
